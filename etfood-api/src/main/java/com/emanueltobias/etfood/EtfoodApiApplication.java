3caf53fa0fbf659286265e17af9c4533fdacfb21
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2021-01-12 00:19:06 -0300

    Criação do projeto

---
+++ etfood-api/src/main/java/com/emanueltobias/etfood/EtfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.emanueltobias.etfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtfoodApiApplication.class, args);
+	}
+
+}
---
+++ etfood-api/src/test/java/com/emanueltobias/etfood/EtfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanueltobias.etfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
