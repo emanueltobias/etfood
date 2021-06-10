62d2497be240ffed97b4dcb8f34628bd7591c2a4
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2021-06-09 23:56:08 -0300

    Implementado repositório SDJ customizado

---
+++ etfood-api/src/main/java/com/emanueltobias/etfood/domain/repository/RestauranteRepositoryQueries.java
@@ -0,0 +1,12 @@
+package com.emanueltobias.etfood.domain.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.emanueltobias.etfood.domain.model.Restaurante;
+
+public interface RestauranteRepositoryQueries {
+
+	List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
+
+}
