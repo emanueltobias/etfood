296e0f3428f3db210011183e38bff66cdb261c92
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2021-08-21 22:29:34 -0300

    Consulta dinâmica com JPQL

--- etfood-api/src/main/java/com/emanueltobias/etfood/api/controller/TesteController.java
+++ etfood-api/src/main/java/com/emanueltobias/etfood/api/controller/TesteController.java
@@ -5,16 +5,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.emanueltobias.etfood.domain.model.Cozinha;
 import com.emanueltobias.etfood.domain.model.Restaurante;
 import com.emanueltobias.etfood.domain.repository.CozinhaRepository;
 import com.emanueltobias.etfood.domain.repository.RestauranteRepository;
 
-@Controller
+@RestController
 @RequestMapping("/teste")
 public class TesteController {
 	
