5ba54ffdb77271c87e724cf9d276a8bca7c86e9b
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2021-10-12 23:26:42 -0300

    Injetado o próprio repositório na implementação customizada, utilizando a anotação @Lazy

--- etfood-api/src/main/java/com/emanueltobias/etfood/api/controller/TesteController.java
+++ etfood-api/src/main/java/com/emanueltobias/etfood/api/controller/TesteController.java
@@ -1,8 +1,5 @@
 package com.emanueltobias.etfood.api.controller;
 
-import static com.emanueltobias.etfood.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
-import static com.emanueltobias.etfood.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
@@ -77,8 +74,7 @@ public class TesteController {
 	@GetMapping("/restaurantes/com-frete-gratis")
 	public List<Restaurante> restaurantesComFreteGratis(String nome) {
 		
-		return restauranteRepository.findAll(comFreteGratis()
-				.and(comNomeSemelhante(nome)));
+		return restauranteRepository.findComFreteGratis(nome);
 	}
 	
 }
