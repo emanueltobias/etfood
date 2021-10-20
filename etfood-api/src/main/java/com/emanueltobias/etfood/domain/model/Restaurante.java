87db9d9eb9dd12622f46321596a68fc394eb61c4
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2021-10-19 23:48:02 -0300

    Correção impacto do relacionamento muitos-para-muitos na REST API

--- etfood-api/src/main/java/com/emanueltobias/etfood/domain/model/Restaurante.java
+++ etfood-api/src/main/java/com/emanueltobias/etfood/domain/model/Restaurante.java
@@ -14,6 +14,8 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -37,10 +39,11 @@ public class Restaurante {
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
 	
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "restaurante_forma_pagamento", 
 		joinColumns = @JoinColumn(name = "restaurante_id"),
 		inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
-	private List<FormaPagamento> formaPagamentoList = new ArrayList<>();
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 
 }
