1c07177ef9467e1d813d326ece4127ea934493b1
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2021-10-23 16:50:54 -0300

    Relacionamento um-para-muitos, classe Restaurante -> classe Produto

--- etfood-api/src/main/java/com/emanueltobias/etfood/api/controller/RestauranteController.java
+++ etfood-api/src/main/java/com/emanueltobias/etfood/api/controller/RestauranteController.java
@@ -74,7 +74,7 @@ public class RestauranteController {
 					.findById(restauranteId).orElse(null);
 			
 			if (restauranteAtual != null) {
-				BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco", "dataCadastro");
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
 				
 				Restaurante restauranteSalvo = cadastroRestauranteService.salvar(restauranteAtual);
 				return ResponseEntity.ok(restauranteSalvo);
