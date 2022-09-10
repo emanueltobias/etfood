15fe3838ef8a3e78eec5b731dc30465af4701ce8
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2022-09-10 12:26:31 -0300

    Customizado exception handlers de ResponseEntityExceptionHandler

--- etfood-api/src/main/java/com/emanueltobias/etfood/api/exceptionhandler/ProblemType.java
+++ etfood-api/src/main/java/com/emanueltobias/etfood/api/exceptionhandler/ProblemType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 	
+	MENSAGEM_INCOMPREENSIVEL("Mensagem incompreensível", "/mensagem-incompreensivel"),
 	ERRO_NEGOCIO("Violação de regra de negócio", "/erro-negocio"),
 	ENTIDADE_NAO_ENCONTRADA("Entidade não encontrada", "/entidade-nao-encontrada"),
 	ENTIDADE_EM_USO("Entidade em uso", "/entidade-em-uso");
