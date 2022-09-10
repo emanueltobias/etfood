877b5aa2aade99866fb2d73c5652fb1f05fe9e95
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2022-09-10 12:12:54 -0300

    Implementado o formato de problemas no corpo das respostas

--- etfood-api/src/main/java/com/emanueltobias/etfood/api/exceptionhandler/ProblemType.java
+++ etfood-api/src/main/java/com/emanueltobias/etfood/api/exceptionhandler/ProblemType.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 	
-	ENTIDADE_NAO_ENCONTRADA("Entidade não encontrada", "/entidade-nao-encontrada");
+	ERRO_NEGOCIO("Violação de regra de negócio", "/erro-negocio"),
+	ENTIDADE_NAO_ENCONTRADA("Entidade não encontrada", "/entidade-nao-encontrada"),
+	ENTIDADE_EM_USO("Entidade em uso", "/entidade-em-uso");
 	
 	private String title;
 	private String uri;
