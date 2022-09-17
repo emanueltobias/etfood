e2a1d193e32e5fdd94346ebbaf6cee54a96d89c6
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2022-09-16 21:56:41 -0300

    Implementado o processo de validação programaticamente

--- etfood-api/src/main/java/com/emanueltobias/etfood/api/controller/RestauranteController.java
+++ etfood-api/src/main/java/com/emanueltobias/etfood/api/controller/RestauranteController.java
@@ -15,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.SmartValidator;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -26,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.emanueltobias.etfood.core.validation.ValidacaoException;
 import com.emanueltobias.etfood.domain.exception.CozinhaNaoEncontradaException;
 import com.emanueltobias.etfood.domain.exception.EntidadeNaoEncontradaException;
 import com.emanueltobias.etfood.domain.exception.NegocioException;
@@ -44,6 +47,9 @@ public class RestauranteController {
 	
 	@Autowired
 	CadastroRestauranteService cadastroRestauranteService;
+	
+	@Autowired
+	private SmartValidator smartValidator;
 	
 	@GetMapping
 	public List<Restaurante> listar() {
@@ -97,8 +103,19 @@ public class RestauranteController {
 	    Restaurante restauranteAtual = cadastroRestauranteService.buscarOuFalhar(idRestaurante);
 	    
 	    merge(campos, restauranteAtual, request);
+	    validate(restauranteAtual, "restaurante");
 	    
 	    return atualizar(idRestaurante, restauranteAtual);
+	}
+	
+	private void validate(Restaurante restaurante, String objetctName) {
+		BeanPropertyBindingResult bindingResult = new BeanPropertyBindingResult(restaurante, objetctName);
+		smartValidator.validate(restaurante, bindingResult);
+		
+		if (bindingResult.hasErrors()) {
+			throw new ValidacaoException(bindingResult);
+		}
+		
 	}
 
 	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
