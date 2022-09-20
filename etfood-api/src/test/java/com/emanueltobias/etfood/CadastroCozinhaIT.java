e5fc946e73ed23be99a65ce32d84a8fba09955cc
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2022-09-19 23:08:35 -0300

    Validado o corpo da resposta HTTP

--- etfood-api/src/test/java/com/emanueltobias/etfood/CadastroCozinhaIT.java
+++ etfood-api/src/test/java/com/emanueltobias/etfood/CadastroCozinhaIT.java
@@ -1,5 +1,6 @@
 package com.emanueltobias.etfood;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -18,6 +19,8 @@ class CadastroCozinhaIT {
 
 	@Test
 	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
 		RestAssured.given()
 			.basePath("/cozinhas")
 			.port(port)
@@ -26,6 +29,21 @@ class CadastroCozinhaIT {
 			.get()
 		.then()
 			.statusCode(HttpStatus.OK.value());
+	}
+	
+	@Test
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		RestAssured.given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", Matchers.hasSize(4))
+			.body("nome", Matchers.hasItems("Tailandesa", "Indiana"));
 	}
 
 }
